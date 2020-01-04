20a15837e55406ede4a8c63279d2ba554bd1fada
gokaykucuk <serdargokaykucuk@gmail.com>
2020-01-04 11:46:56 +0100

    cities created

--- db/create_cities.js
+++ db/create_cities.js
@@ -4,29 +4,30 @@ const q = faunadb.query;
 const citiesData = require("./data/cities.json");
 
 const Seeder = {
-  seed: async () => {
-    const serverClient = new faunadb.Client({
-      secret: process.env.FAUNADB_SERVER_KEY
-    });
+	seed: async () => {
+		const serverClient = new faunadb.Client({
+			secret: 'fnADhQm_vDACC0g2DkY2s8dvU4Kf5d6lrEvi8T1Q'
+		});
 
-    citiesData.forEach(async cityData => {
-      try {
-        await serverClient.query(
-          q.Create("cities", {
-            data: {
-              name: cityData.city,
-              name_eng: cityData.city_ascii,
-              lat: cityData.lat,
-              lng: cityData.lng
-            }
-          })
-        );
-        console.log(`City ${cityParams.name} created.`);
-      } catch (e) {
-        console.log(e);
-      }
-    });
-  }
+		for (let i = 0; i < citiesData.length; i++) {
+			const cityData = citiesData[i];
+			try {
+				await serverClient.query(
+					q.Create("cities", {
+						data: {
+							name: cityData.city,
+							name_eng: cityData.city_ascii,
+							lat: cityData.lat,
+							lng: cityData.lng
+						}
+					})
+				);
+				console.log(`City ${cityData.city} created.`);
+			} catch (e) {
+				console.log(e);
+			}
+		}
+	}
 };
 
 exports.Seeder = Seeder;
